16c54c47b860b8e07feebff38993f4fbe58f5b3b
austin smythe <aust.burner@gmail.com>
2020-02-13 09:07:59 -0700

    first commit

---
+++ src/bootstrap.js
@@ -0,0 +1,10 @@
+function summingTool(numbers) {
+    let sum = 0
+    for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i]
+    }
+    return sum
+    console.log(sum)
+  }
+  
+  console.log(summingTool([1, 2, 3, 4, 5, 6]))
